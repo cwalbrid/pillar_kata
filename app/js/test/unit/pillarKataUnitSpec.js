f5263e1d9bffa5909a99923eb1478ab710b31311
Colin Walbridge <cwalbrid@umich.edu>
2017-03-30 13:59:47 -0400

    added basic beforeEach and afterEach functionality to pillarKataUnitSpec.js

--- app/js/test/unit/pillarKataUnitSpec.js
+++ app/js/test/unit/pillarKataUnitSpec.js
@@ -1,17 +1,20 @@
 describe('Testing AngularJS Test Suite', function(){
+	beforeEach(module('pillarApp'));
+
+
 	describe('Testing mainCtrl', function(){
+		var scope, ctrl;
+		
+		beforeEach(inject(function($controller, $rootScope){
+				scope = $rootScope.$new();
+				ctrl = $controller('mainCtrl', {$scope:scope});
+			}));
+
+		afterEach(function(){
+			//cleanup code
+		});
 
 		it('should initialize the title in the scope', function(){
-
-			module('pillarApp');
-
-			var scope = {};
-			var ctrl;
-
-			inject(function($controller){
-				ctrl = $controller('mainCtrl', {$scope:scope});
-			});
-
 			expect(scope.title).toBeDefined();
 			expect(scope.title).toBe('Pillar Kata');
 		});
