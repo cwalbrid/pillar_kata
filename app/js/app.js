1c6d82dd60c6bc8ed1097c585bf4c75931702183
Colin Walbridge <cwalbrid@umich.edu>
2017-04-25 15:34:26 -0400

    linked to app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,6 @@
-// alert('whatup fam?')
 var app = angular.module('pillarApp', []);
 
 app.controller('mainCtrl', function($rootScope, $scope){
 
-	$scope.title = 'Pillar Kata'
+	$scope.title = 'Pillar Kata';
 });
